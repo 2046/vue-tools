4647cdf9176c4d6f94699abd0a273378d26384b6
2046 <g@ggg.gd>
2016-10-21 13:30:16 +0800

    replace layout component

--- examples/entry.js
+++ examples/entry.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Layout from './layout'
+import Layout from 'components/layout'
 
 Vue.component('Layout', Layout)
 
