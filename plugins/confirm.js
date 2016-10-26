108ad9b2478f5732d49a02dbeb9e52e4fc7adc5a
2046 <g@ggg.gd>
2016-10-26 11:29:05 +0800

    add params

--- plugins/confirm.js
+++ plugins/confirm.js
@@ -18,10 +18,33 @@ function plugin(Vue) {
     document.getElementsByTagName('body')[0].appendChild(container)
     component = new Component({propsData}).$mount(container)
 
-    function method(title = '', text = '', onHide = noop) {
+    function method(title = '', text = '', buttonOkText, buttonCancelText, onHide = noop) {
+        if(typeof buttonOkText === 'function') {
+            onHide = buttonOkText
+            buttonOkText = ''
+        }
+
+        if(typeof buttonCancelText === 'function') {
+            onHide = buttonCancelText
+            buttonCancelText = ''
+        }
+
         component.text = text
         component.title = title
         component.visible = true
+
+        if(buttonOkText) {
+            component.buttonOkText = buttonOkText
+        }else {
+            component.buttonOkText = '确定'
+        }
+
+        if(buttonCancelText) {
+            component.buttonCancelText = buttonCancelText
+        }else {
+            component.buttonCancelText = '取消'
+        }
+
         component.$on('hide', (bool) => {
             onHide(bool)
             component.visible = false
