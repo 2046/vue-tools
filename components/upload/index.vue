f58fd412978a89d8d50f73daf3c325e7641c2d3e
jiangtao <321jiangtao@gmail.com>
2016-11-13 02:10:44 +0800

    add upload component, depend on Imager

---
+++ components/upload/index.vue
@@ -0,0 +1,87 @@
+<template>
+    <input type="file" @change="change($event)"/>
+</template>
+
+<script>
+    import Imager from './Imager'
+    import ajax from 'plugins/ajax'
+    import Vue from 'vue'
+    Vue.use(ajax)
+
+    export default {
+        props: {
+            url: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            compress: {
+                type: Boolean
+            },
+            base64: {
+                type: Boolean
+            }
+        },
+        data(){
+            return {
+                src: ''
+            }
+        },
+        methods: {
+            change(e){
+                let file, image
+
+                file = e.target.files[0]
+
+                if(!file) {
+                    return
+                }
+                this.$emit('change', e)
+
+                if(Imager.isImage(file) && this.compress) {
+                    image = new Imager({
+                        file: file,
+                        quality: 0.8
+                    })
+                    image.getExif().then(data => {
+                        return image.loaded()
+                    }).then(base64 => {
+                        if(this.base64) {
+                            this.upload(base64)
+                        } else {
+                            this.upload(Imager.base64ToFile(base64, file.name))
+                        }
+                    })
+                } else {
+                    this.upload(file)
+                }
+
+            },
+            upload(file){
+                if(this.url) {
+                    let fd = new FormData()
+                    this.name = this.name || 'file'
+                    fd.append('uid', 'P25y44Lc2ryNzJ5m2')
+                    fd.append('type', 'attach')
+                    fd.append(this.name, file)
+                    Vue.http.post(this.url, fd, {
+                        progress: e => {
+                            this.$emit('progress', e)
+                        }
+                    }).then(response => {
+                        console.log(response)
+                        this.$emit('upload', response)
+                    }).catch(response => {
+                        this.$emit('upload', response)
+                    })
+                } else {
+                    this.$emit('upload', file)
+                    this.$emit('progress', function() {
+                        console.warn('if url was not input, progress not work')
+                    })
+                }
+            }
+        }
+    }
+</script>
